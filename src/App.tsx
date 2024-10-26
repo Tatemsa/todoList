27a0edad06615769ad8d5c5c82f3f8c29ae23890
BILL <tatemsabill@gmail.com>
2024-10-26 17:52:20 +0200

    initialisation du projet de réalisation d'une todo List

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <button className='button'>Mon bouton de bootstrap</button>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
